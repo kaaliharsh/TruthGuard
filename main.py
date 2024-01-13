cc2fc72f045e1a1b525bc9aa5f04484a8883cdb2
Harsh Deep <108045259+kaaliharsh@users.noreply.github.com>
2024-01-13 12:57:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score  
 
 #-----------------------------Load data----------------------------------------
 
