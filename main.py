aab03b1df5b443b7781bec62c6d501bafc3eb220
Harsh Deep <108045259+kaaliharsh@users.noreply.github.com>
2024-01-16 23:35:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score  
 
-
  
 #-----------------------------Load data----------------------------------------
 
