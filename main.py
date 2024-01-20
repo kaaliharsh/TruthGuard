82adab3bb0a6644b9c3755fd48070d1b2daf032c
Harsh Deep <108045259+kaaliharsh@users.noreply.github.com>
2024-01-20 23:47:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score  
-  
+   
     
 #-----------------------------Load data----------------------------------------
 
