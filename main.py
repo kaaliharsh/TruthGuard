66b43e6a23843ca758fe15af15988597549f001e
Harsh Deep <108045259+kaaliharsh@users.noreply.github.com>
2024-01-20 23:47:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score  
    
-    
+       
 #-----------------------------Load data----------------------------------------
 
 news_df = pd.read_csv('train.csv') 
